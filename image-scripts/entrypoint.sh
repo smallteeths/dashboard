c929cdfb30d7ec1618bd72dc76478872e7e08aa3
root <you@example.com>
2021-08-04 11:34:50 +0800

    modify Dockerfile

--- image-scripts/entrypoint.sh
+++ image-scripts/entrypoint.sh
@@ -2,6 +2,7 @@
 
 find /usr/share/nginx/html -name '*.html' -o -name '*.js' | xargs sed -i.bak -e 's/releases.rancher.com\//'$HOST'\//g'
 find /usr/share/nginx/html -name '*.bak' | xargs rm -rf
-envsubst '${ALLOW_ORIGIN}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '${ALLOW_ORIGIN}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/nginx.conf
 
 nginx -g 'daemon off;'
+
