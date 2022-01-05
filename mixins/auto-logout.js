843ae511bd03619bd7a46279830e17a140540a6b
LiuYan <361112237@qq.com>
2022-01-05 16:00:58 +0800

    fix(auto-logout): fix auto logout error

--- mixins/auto-logout.js
+++ mixins/auto-logout.js
@@ -1,6 +1,7 @@
 import { SETTING } from '@/config/settings';
 import { MANAGEMENT } from '@/config/types';
 import throttle from 'lodash/throttle';
+import { LOGGED_OUT } from '@/config/query-params';
 
 const defaultEvents = ['mousemove', 'mousedown', 'resize', 'keydown', 'touchstart', 'wheel'];
 
@@ -8,20 +9,23 @@ export default {
   async fetch() {
     const uiSessionLogoutMinutes = await this.$store.getters['management/byId'](MANAGEMENT.SETTING, SETTING.UI_SESSION_LOGOUT_MINUTES);
 
-    this.uiSessionLogoutMinutes = uiSessionLogoutMinutes?.value ?? 30;
+    this.uiSessionLogoutMinutes = uiSessionLogoutMinutes;
   },
 
   data() {
     return {
       lastActive:             new Date().getTime(),
       autoLogoutTimer:        null,
-      uiSessionLogoutMinutes: 30,
+      uiSessionLogoutMinutes: null,
     };
   },
 
   computed: {
     uiSessionTimeout() {
-      return parseInt(this.uiSessionLogoutMinutes, 10) * 1000 * 60;
+      return parseInt(this.sessionLogoutMinutes, 10) * 1000 * 60;
+    },
+    sessionLogoutMinutes() {
+      return this.uiSessionLogoutMinutes?.value ?? 30;
     }
   },
 
@@ -35,11 +39,11 @@ export default {
 
       this.$store.dispatch('growl/warning', {
         title:   this.t('autoLogout.title'),
-        message: this.t('autoLogout.message', { autoLogoutTime: this.uiSessionLogoutMinutes })
+        message: this.t('autoLogout.message', { autoLogoutTime: this.sessionLogoutMinutes })
       }, { root: true });
 
       window.setTimeout(() => {
-        this.$router.push({ name: 'auth-logout' });
+        this.$router.push({ name: 'auth-logout', query: { [LOGGED_OUT]: true } });
       }, 5000);
     },
 
