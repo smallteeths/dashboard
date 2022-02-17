b24b12e62ce0d93c0f6051e41b66e88af506e58a
Siye Wang <siye@rancher.com>
2022-02-17 10:47:44 +0800

    feat(webhook):Support pandaria webhook - Ding Talk - Ms Teams - Aliyun SMS - Service Now

--- edit/monitoring.coreos.com.receiver/index.vue
+++ edit/monitoring.coreos.com.receiver/index.vue
@@ -1,4 +1,5 @@
 <script>
+import { isEmpty, cloneDeep } from 'lodash';
 import { MONITORING } from '@/config/types';
 import ArrayListGrouped from '@/components/form/ArrayListGrouped';
 import Loading from '@/components/Loading';
@@ -9,9 +10,13 @@ import Tabbed from '@/components/Tabbed';
 import Tab from '@/components/Tabbed/Tab';
 import YamlEditor, { EDITOR_MODES } from '@/components/YamlEditor';
 import CreateEditView from '@/mixins/create-edit-view';
+import { loadAlertingConfig } from '@/utils/alertmanagerconfig';
 import jsyaml from 'js-yaml';
-import { RECEIVERS_TYPES } from '@/models/monitoring.coreos.com.receiver';
+import {
+  RECEIVERS_TYPES, PANDARIAWEBHOOKKEY, WEBHOOKKEY, PANDARIAWEBHOOKURL, PANDARIADEFAULTTYPE
+} from '@/models/monitoring.coreos.com.receiver';
 import ButtonDropdown from '@/components/ButtonDropdown';
+import { allHash } from '@/utils/promise';
 
 export default {
   components: {
@@ -29,7 +34,51 @@ export default {
   mixins: [CreateEditView],
 
   async fetch() {
-    await this.$store.dispatch('cluster/findAll', { type: MONITORING.SPOOFED.ROUTE });
+    const { $store } = this;
+    const hash = await allHash({
+      rotes:           $store.dispatch('cluster/findAll', { type: MONITORING.SPOOFED.ROUTE }),
+      alertingDrivers: loadAlertingConfig($store.dispatch),
+    });
+    const pandariaWebhookConfigs = this.value.spec[PANDARIAWEBHOOKKEY];
+
+    if (!isEmpty(hash.alertingDrivers?.config)) {
+      this.alertingDrivers = hash.alertingDrivers?.config;
+    }
+
+    // Pandaria. Convert to pandaria webhook config
+    if (this.alertingDrivers) {
+      const providers = this.alertingDrivers.providers;
+      const receivers = this.alertingDrivers.receivers;
+
+      if (pandariaWebhookConfigs?.length) {
+        pandariaWebhookConfigs.forEach((item) => {
+          if (item.url.startsWith(PANDARIAWEBHOOKURL)) {
+            const key = item.url.replace(PANDARIAWEBHOOKURL, '');
+            const providerName = receivers[key]?.provider;
+
+            if (providerName) {
+              const receiver = providers[providerName];
+
+              // Pandaria alerting providers convert to webhook config
+              item.http_config = {};
+              Object.assign(item.http_config, receiver);
+              item.type = receiver.type;
+              item.webhook_url = receiver.webhook_url;
+              delete item.http_config?.type;
+              delete item.http_config?.webhook_url;
+
+              // Pandaria AliCloud SMS alerts have an array of phone numbers that are special.
+              // It needs to be retrieved from alerting-drivers secret receivers.to
+              if (receivers[key]?.to?.length > 0) {
+                item.http_config.phone = receivers[key].to;
+              }
+            } else {
+              item.type = PANDARIADEFAULTTYPE;
+            }
+          }
+        });
+      }
+    }
   },
 
   data() {
@@ -49,6 +98,22 @@ export default {
       }
     });
 
+    // Pandaria extra webhook
+    Object.keys(this.value.spec).forEach((key) => {
+      if (key === WEBHOOKKEY && this.value.spec[key]?.length > 0) {
+        const newData = [];
+
+        this.value.spec[key].forEach((item) => {
+          if (item?.url?.startsWith(PANDARIAWEBHOOKURL)) {
+            suffix[PANDARIAWEBHOOKKEY]?.push(item);
+          } else {
+            newData.push(item);
+          }
+        });
+        this.value.spec[key] = newData;
+      }
+    });
+
     let suffixYaml = jsyaml.dump(suffix);
 
     if (suffixYaml.trim() === '{}') {
@@ -57,7 +122,7 @@ export default {
 
     return {
       expectedFields,
-      receiverTypes:        RECEIVERS_TYPES,
+      receiverTypes:        cloneDeep(RECEIVERS_TYPES),
       fileFound:            false,
       receiver:             {},
       suffixYaml,
