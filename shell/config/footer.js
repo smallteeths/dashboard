fd9dd7b9cdfa1aa2dd96e5e9db92f78d881b1151
huacheng <cheng@HuachengdeMacBook-Pro.local>
2022-11-15 19:54:31 +0800

    fix(Community Support): Update link address

--- shell/config/footer.js
+++ shell/config/footer.js
@@ -1,5 +1,3 @@
-import { DOCS_BASE } from '@shell/config/private-label';
-
 export function options(issues, hideRancher) {
   if (!issues) {
     if (hideRancher) {
@@ -11,10 +9,10 @@ export function options(issues, hideRancher) {
   }
 
   return {
-    'footer.docs':    DOCS_BASE,
-    'footer.forums': 'https://forums.rancher.com/',
+    'footer.docs':   'https://ee.docs.rancher.cn/',
+    'footer.forums': 'https://forums.rancher.cn/',
     'footer.slack':  'https://slack.rancher.io',
     'footer.issue':  issues,
-    'footer.mirror': 'http://mirror.cnrancher.com',
+    'footer.mirror': 'https://mirror.rancher.cn/',
   };
 }
