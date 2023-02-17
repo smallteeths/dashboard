ebc567ce1231f5dfd31b2190a29e7a80ffda8094
LiuYan <361112237@qq.com>
2023-02-17 15:48:38 +0800

    feat(agentEnv): add http proxy env tips

--- shell/edit/provisioning.cattle.io.cluster/AgentEnv.vue
+++ shell/edit/provisioning.cattle.io.cluster/AgentEnv.vue
@@ -1,9 +1,13 @@
 <script>
 import KeyValue from '@shell/components/form/KeyValue';
 import Tab from '@shell/components/Tabbed/Tab';
+import Markdown from '@shell/components/Markdown';
+import { Banner } from '@components/Banner';
 
 export default {
-  components: { KeyValue, Tab },
+  components: {
+    KeyValue, Tab, Markdown, Banner
+  },
 
   props: {
     mode: {
@@ -16,11 +20,24 @@ export default {
       required: true,
     },
   },
+  data() {
+    return {
+      example: `
+      HTTP_PROXY=http://\${ proxy_host }
+      HTTPS_PROXY=http://\${ proxy_host }
+      NO_PROXY=127.0.0.0/8,10.0.0.0/8,cattle-system.svc,172.16.0.0/12,192.168.0.0/16`
+    };
+  }
 };
 </script>
 
 <template>
   <Tab name="agentEnv" label-key="cluster.tabs.agentEnv">
+    <Banner color="info">
+      {{ t('cluster.agentEnvVars.tips') }} For example:
+      <Markdown :value="example" />
+    </Banner>
+
     <KeyValue
       v-model="value.spec.agentEnvVars"
       :mode="mode"
