412b7935aee89e86a3692c5b044596949f99abd1
huacheng <cheng@HuachengdeMacBook-Pro.local>
2023-03-16 13:55:42 +0800

    fix(logging): Support TKE

--- shell/chart/logging/index.vue
+++ shell/chart/logging/index.vue
@@ -1,17 +1,60 @@
 <script>
 import { mapGetters } from 'vuex';
 import { LabeledInput } from '@components/Form/LabeledInput';
+import LabeledSelect from '@shell/components/form/LabeledSelect';
 import { Checkbox } from '@components/Form/Checkbox';
 
 export default {
-  components: { Checkbox, LabeledInput },
-  props:      {
+  components: {
+    Checkbox, LabeledInput, LabeledSelect
+  },
+  props: {
     value: {
       type:    Object,
       default: () => {
         return {};
       }
     }
+  },
+
+  data() {
+    return {
+      clusterType:  this.value?.loggingOverlay?.spec?.fluentbit ? 'tke' : '',
+      clusterTypes: [{
+        label: this.t('logging.install.clusterType.default'),
+        value: '',
+      }, {
+        label: this.t('logging.install.clusterType.tke'),
+        value: 'tke'
+      }]
+    };
+  },
+
+  watch: {
+    provider(neu) {
+      this.$set(this, 'clusterType', neu === 'tke' ? 'tke' : '');
+    },
+    clusterType(neu) {
+      if (neu === 'tke') {
+        const loggingOverlay = {
+          spec: {
+            fluentbit: {
+              extraVolumeMounts: [
+                {
+                  destination: '/var/lib/containerd',
+                  readOnly:    true,
+                  source:      '/var/lib/containerd'
+                }
+              ]
+            }
+          }
+        };
+
+        this.$set(this.value, 'loggingOverlay', loggingOverlay);
+      } else {
+        this.value.loggingOverlay && this.$set(this.value, 'loggingOverlay', {});
+      }
+    },
   },
 
   computed: {
@@ -39,7 +82,21 @@ export default {
 
 <template>
   <div class="logging">
-    <div v-if="provider === 'k3s'" class="row mb-20">
+    <div class="row mb-20">
+      <div class="col span-6">
+        <LabeledSelect
+          v-model="clusterType"
+          :label="t('logging.install.clusterType.label')"
+          :placeholder="t('logging.install.clusterType.placeholder')"
+          :localized-label="true"
+          :options="clusterTypes"
+        />
+      </div>
+    </div>
+    <div
+      v-if="provider === 'k3s' && clusterType === ''"
+      class="row mb-20"
+    >
       <div class="col span-6">
         <LabeledInput v-model="value.additionalLoggingSources.k3s.container_engine" :label="t('logging.install.k3sContainerEngine')" />
       </div>
