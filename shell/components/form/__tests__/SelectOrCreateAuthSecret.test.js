54f0580432d5b5c20f173f5772b357c86aed472c
huacheng <cheng@HuachengdeMacBook-Pro.local>
2023-06-12 14:02:39 +0800

    test(cluster registry): Fix create auth secret

---
+++ shell/components/form/__tests__/SelectOrCreateAuthSecret.test.js
@@ -0,0 +1,25 @@
+import SelectOrCreateAuthSecret from '@shell/components/form/SelectOrCreateAuthSecret.vue';
+import { base64Encode } from '@shell/utils/crypto';
+
+describe('component: SelectOrCreateAuthSecret, computed: options', () => {
+  it('should return anno display-name options', () => {
+    const host = 'a.b.c:8080';
+    const item = {
+      id:        'id1',
+      namespace: 'test',
+      metadata:  { namespace: 'test', annotations: { 'display-name': base64Encode(host) } }
+    };
+    const localThis = {
+      t:          (t) => t,
+      allSecrets: [item]
+    };
+
+    const options = SelectOrCreateAuthSecret.computed.options.call(localThis);
+
+    expect(options[options.length - 1]).toStrictEqual({
+      label: `${ host } (${ item.subTypeDisplay }: ${ item.dataPreview })`,
+      group: item.metadata.namespace,
+      value: item.id,
+    });
+  });
+});
--- shell/edit/provisioning.cattle.io.cluster/__tests__/rke2.test.js
+++ shell/edit/provisioning.cattle.io.cluster/__tests__/rke2.test.js
@@ -1,17 +1,15 @@
 import rke2 from '@shell/edit/provisioning.cattle.io.cluster/rke2.vue';
 
 describe('component: rke2, computed: generateName', () => {
-  const defaultGenerateName = 'registryconfig-auth-';
-
   it('should return default generate name when empty registry hostname', () => {
     const localThis = { registryHost: '' };
 
-    expect(rke2.computed.generateName.call(localThis)).toBe(defaultGenerateName);
+    expect(rke2.computed.generateName.call(localThis)).toBe('');
   });
 
   it('should return registry hostname generate name', () => {
     const localThis = { registryHost: 'a.a.a' };
 
-    expect(rke2.computed.generateName.call(localThis)).toBe(`${ localThis.registryHost }-`);
+    expect(rke2.computed.generateName.call(localThis)).toBe(localThis.registryHost);
   });
 });
