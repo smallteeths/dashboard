531d7af0a42b6732740add3d8ae39163910ee581
LiuYan <361112237@qq.com>
2023-07-26 18:31:34 +0800

    fix(model/pod.js): fix node link jump error if no permission

--- shell/store/type-map.js
+++ shell/store/type-map.js
@@ -940,6 +940,9 @@ export const getters = {
 
   headersFor(state, getters, rootState, rootGetters) {
     return (schema) => {
+      if (!schema) {
+        return [];
+      }
       const attributes = schema.attributes || {};
       const columns = attributes.columns || [];
       const typeOptions = getters['optionsFor'](schema);
