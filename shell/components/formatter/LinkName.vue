7006bac1cb3e47680cae9339d3eaaf23f9309602
LiuYan <361112237@qq.com>
2023-07-27 13:56:38 +0800

    fix(LinkName): fix node link error for pod list

--- shell/components/formatter/LinkName.vue
+++ shell/components/formatter/LinkName.vue
@@ -1,5 +1,6 @@
 <script>
 import { NAME as EXPLORER } from '@shell/config/product/explorer';
+import { NODE as NODE_TYPE, MANAGEMENT } from '@shell/config/types';
 
 export default {
   props: {
@@ -36,6 +37,13 @@ export default {
       };
 
       return { name, params };
+    },
+    hasPermission() {
+      if (this.type === NODE_TYPE) {
+        return this.$store.getters['management/schemaFor'](MANAGEMENT.NODE);
+      }
+
+      return true;
     }
   }
 };
@@ -43,8 +51,14 @@ export default {
 
 <template>
   <span v-if="value">
-    <nuxt-link :to="url">
+    <nuxt-link
+      v-if="hasPermission"
+      :to="url"
+    >
       {{ value }}
     </nuxt-link>
+    <template v-else>
+      {{ value }}
+    </template>
   </span>
 </template>
