d189a95fea1db95952b31fff6692104f90461915
ly5156 <361112237@qq.com>
2024-09-18 14:20:10 +0800

    fix(f5-cis): show no permission tips

--- pkg/f5-cis/pages/index.vue
+++ pkg/f5-cis/pages/index.vue
@@ -1,9 +1,15 @@
 <template>
   <PageSwitch>
-    <div />
+    <div>
+      <Banner
+        color="info"
+        label-key="f5cis.noPermission"
+      />
+    </div>
   </PageSwitch>
 </template>
 <script>
 import PageSwitch from '../components/PageSwitch.vue';
-export default { components: { PageSwitch } };
+import Banner from '@components/Banner/Banner.vue';
+export default { components: { PageSwitch, Banner } };
 </script>
