d9f10062f0abb2d2125ec46004a212313ffaceaa
ly5156 <361112237@qq.com>
2024-12-27 16:29:20 +0800

    fix(hami): Fix page jump error if user don't have access to the app catalog api

--- pkg/hami/components/PageSwitch.vue
+++ pkg/hami/components/PageSwitch.vue
@@ -27,7 +27,7 @@ export default {
   },
   data() {
     return {
-      apps: [], loading: true, errors: [], requiredApps: ['rancher-monitoring', 'rancher-hami']
+      apps: [], loading: true, errors: [], hamiResourceTypes: null, requiredApps: ['rancher-monitoring', 'rancher-hami']
     };
   },
   created() {
@@ -38,9 +38,22 @@ export default {
       this.loading = true;
       try {
         const id = this.$route.params.cluster;
-        const { data: apps } = await this.$store.dispatch('management/request', { url: `/k8s/clusters/${ id === '_' ? 'local' : id }/v1/catalog.cattle.io.apps` });
+        const inStore = this.$store.getters['currentProduct'].inStore;
+        const hamiResourceTypesPromise = this.$store.dispatch(
+          `${ inStore }/request`,
+          { url: `/k8s/clusters/${ id === '_' ? 'local' : id }/v1/hami.pandaria.com.resourcetypes/rancher-hami-resourcetypes` }
+        );
+        const appsPromise = this.$store.dispatch('management/request', { url: `/k8s/clusters/${ id === '_' ? 'local' : id }/v1/catalog.cattle.io.apps` });
 
-        this.apps = apps;
+        const [hamiResourceTypesResult, appsResult] = await Promise.allSettled([hamiResourceTypesPromise, appsPromise]);
+
+        if (hamiResourceTypesResult.status === 'fulfilled') {
+          this.hamiResourceTypes = hamiResourceTypesResult.value;
+        }
+
+        if (appsResult.status === 'fulfilled') {
+          this.apps = appsResult.value?.data ?? [];
+        }
       } catch (err) {
         this.errors = [stringify(err)];
       }
@@ -56,6 +69,10 @@ export default {
       return !!this.installedApps.find((app) => app.spec?.chart?.metadata?.name === 'rancher-monitoring');
     },
     harmiInstalled() {
+      if (this.hamiResourceTypes?.metadata?.labels?.['hami.pandaria.com/installed-by-chart'] === 'true') {
+        return true;
+      }
+
       return !!this.installedApps.find((app) => app.spec?.chart?.metadata?.name === 'rancher-hami');
     },
     installedApps() {
